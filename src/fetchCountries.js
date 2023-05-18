6d556c0db20c285e6af0f990fc51e0f49772711f
Denys Strashniuk <denysstrashniuk@MacBook-Pro-Denys-Strashniuk.local>
2023-05-18 20:48:04 +0200

    add fetchCountries.js

---
+++ src/fetchCountries.js
@@ -0,0 +1,19 @@
+let countriesName = '';
+const urlAPI = 'https://restcountries.com/v3.1/name/';
+const fields = 'name,capital,population,flags,languages';
+
+export default function fetchCountries(countriesName) {
+  return fetch(`${urlAPI}${countriesName}?fields=${fields}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json;
+    })
+    .then(data => {
+      console.log();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+import fetchCountriesAPI from './fetchCountries';
+
+fetchCountriesAPI('ar');
+
+const refs = {
+    searchInput = document.querySelector("#search-box"),
+}
